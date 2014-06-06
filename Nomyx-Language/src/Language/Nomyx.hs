007733ad24f39d08fdb36992edd6b0f6faba9c6b
Corentin Dupont <corentin.dupont@gmail.com>
2014-06-06 16:03:51 +0200

    remove the "imports as X" which are not supported correctly by Haddock

--- Nomyx-Language/src/Language/Nomyx.hs
+++ Nomyx-Language/src/Language/Nomyx.hs
@@ -1,13 +1,21 @@
 {-# LANGUAGE Trustworthy #-}
 
 -- | This module re-exports the elements necessary to compose a Nomyx rule.
-module Language.Nomyx (module X)  where
+module Language.Nomyx (
+   module Language.Nomyx.Expression,
+   module Language.Nomyx.Outputs,
+   module Language.Nomyx.Inputs,
+   module Language.Nomyx.Events,
+   module Language.Nomyx.Players,
+   module Language.Nomyx.Variables,
+   module Language.Nomyx.Rules,
+   module Language.Nomyx.Vote)  where
 
-import Language.Nomyx.Expression as X -- Nomyx Expression DSL
-import Language.Nomyx.Outputs    as X -- create outputs
-import Language.Nomyx.Inputs     as X -- create inputs
-import Language.Nomyx.Events     as X -- create events
-import Language.Nomyx.Players    as X -- manage players
-import Language.Nomyx.Variables  as X -- create variables
-import Language.Nomyx.Rules      as X -- manage rules
-import Language.Nomyx.Vote       as X -- create votations
+import Language.Nomyx.Expression -- Nomyx Expression DSL
+import Language.Nomyx.Outputs    -- create outputs
+import Language.Nomyx.Inputs     -- create inputs
+import Language.Nomyx.Events     -- create events
+import Language.Nomyx.Players    -- manage players
+import Language.Nomyx.Variables  -- create variables
+import Language.Nomyx.Rules      -- manage rules
+import Language.Nomyx.Vote       -- create votations
