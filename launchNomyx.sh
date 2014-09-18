1fd21c1540fff592a531d9147f47051cf6f4c896
Corentin Dupont <corentin.dupont@gmail.com>
2014-09-18 16:27:10 +0200

    better launch

--- launchNomyx.sh
+++ launchNomyx.sh
@@ -1,1 +1,8 @@
-screen -dmS "cabal exec .cabal-sandbox/bin/Nomyx -- -h www.nomyx.net -p 9998 -r . -f ./.cabal-sandbox/share/x86_64-linux-ghc-7.6.2/Nomyx-Web-0.7.0/ -s ./.cabal-sandbox/share/doc/x86_64-linux-ghc-7.6.2/Nomyx-Language-0.7.0/"
+
+
+cmd="cabal exec .cabal-sandbox/bin/Nomyx -- -h www.nomyx.net -p 9998 -r . -f ./.cabal-sandbox/share/x86_64-linux-ghc-7.6.2/Nomyx-Web-0.7.0/ -s ./.cabal-sandbox/share/doc/x86_64-linux-ghc-7.6.2/Nomyx-Language-0.7.0/"
+
+screen -S nomyx -X quit
+screen -dmS nomyx $cmd
+
+
