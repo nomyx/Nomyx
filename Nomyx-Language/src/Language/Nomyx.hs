49a9ab0167c81fcc2dd0fa55864900972e421b5b
kau <kau@kau-Studio-XPS-1647>
2014-10-14 19:29:37 +0200

    cosmetics

--- Nomyx-Language/src/Language/Nomyx.hs
+++ Nomyx-Language/src/Language/Nomyx.hs
@@ -2,16 +2,15 @@
 
 -- | This module re-exports the elements necessary to compose a Nomyx rule.
 module Language.Nomyx (
-   module Language.Nomyx.Expression,
    module Language.Nomyx.Outputs,
    module Language.Nomyx.Inputs,
    module Language.Nomyx.Events,
    module Language.Nomyx.Players,
    module Language.Nomyx.Variables,
    module Language.Nomyx.Rules,
-   module Language.Nomyx.Vote)  where
+   module Language.Nomyx.Vote,
+   module Language.Nomyx.Expression)  where
 
-import Language.Nomyx.Expression -- Nomyx Expression DSL
 import Language.Nomyx.Outputs    -- create outputs
 import Language.Nomyx.Inputs     -- create inputs
 import Language.Nomyx.Events     -- create events
@@ -19,3 +18,4 @@ import Language.Nomyx.Players    -- manage players
 import Language.Nomyx.Variables  -- create variables
 import Language.Nomyx.Rules      -- manage rules
 import Language.Nomyx.Vote       -- create votations
+import Language.Nomyx.Expression -- Nomyx Expression DSL
