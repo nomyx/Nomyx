590b2d1b18d8f3f311059e9943401e74f1966e06
kau <kau@kau-Studio-XPS-1647>
2014-11-08 12:14:15 +0100

    rename agora -> forum

--- Nomyx-Web/src/Nomyx/Web/Help.hs
+++ Nomyx-Web/src/Nomyx/Web/Help.hs
@@ -20,7 +20,7 @@ events = "Rules can register on events, in order to be triggered when the event 
 variables = "Variables: Rules can create variables to store data. For example, a rule creating a bank account with instruction \"newVar\" will make a new variable appear here."
 
 view = "Only view a game. You will not be able to propose new rules."
-joinGame = "Be part of the game. You will be able to propose new rules, vote etc. Please register in the game's agora (see the link on game page) to follow the game."
+joinGame = "Be part of the game. You will be able to propose new rules, vote etc. Please register in the game's forum (see the link on game page) to follow the game."
 
 fork = "This will create a new game based on the previous one. You will be able to test \n" ++
         "your new rules independently of the original game. The new game is completely private: you will be alone. Please delete it when finished."
