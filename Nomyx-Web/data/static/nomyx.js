3059f92cce223b953fc4dc35992e2242c127d876
kau <kau@kau-Studio-XPS-1647>
2014-11-11 23:29:27 +0100

    view game using javascript

--- Nomyx-Web/data/static/nomyx.js
+++ Nomyx-Web/data/static/nomyx.js
@@ -13,3 +13,20 @@ function toggle_visibility(id_events, id_show)
       show.innerHTML ='[Click to show]';
    }
 }
+
+//toggle visibility for overlapping divs
+//all elements with the class name will be pushed back, 
+//while only the element with the id name is put on front
+function div_visibility(id_name, class_name)
+{
+   //push back every divs
+   var divs = document.getElementsByClassName(class_name);
+   for (i = 0; i < divs.length; i++) {
+      divs[i].style.zIndex = 0;
+   }
+
+   //pull front our div
+   var myDiv = document.getElementById(id_name);
+   myDiv.style.zIndex = 1;
+
+}
