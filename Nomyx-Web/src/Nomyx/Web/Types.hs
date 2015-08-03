1e2dbfe74783a26391e0eaa107df5fc9587aa2cc
Corentin Dupont <cdupont@create-net.org>
2015-08-04 00:20:14 +0200

    new file Web.Types

---
+++ Nomyx-Web/src/Nomyx/Web/Types.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module Nomyx.Web.Types where
+
+import           Control.Concurrent.STM
+import           Control.Lens
+import           Control.Monad.State
+import           Data.Acid                     (AcidState)
+import           Data.String                   (fromString)
+import           Data.Text                     (Text, pack)
+import           Happstack.Authenticate.Core   (AuthenticateState,
+                                                AuthenticateURL (..))
+import           Happstack.Server              as HS (Input, Response,
+                                                      ServerPartT)
+import           Language.Nomyx
+import           Nomyx.Core.Engine
+import           Nomyx.Core.Types              as T
+import           Text.Blaze.Html5              hiding (base, map, output)
+import           Text.Reform                   (CommonFormError, ErrorInputType,
+                                                Form, FormError (..))
+import           Text.Reform.Blaze.String      ()
+import           Web.Routes.PathInfo
+import           Web.Routes.RouteT
+import           Web.Routes.TH                 (derivePathInfo)
+
+data NomyxError = PlayerNameRequired
+                | GameNameRequired
+                | UniqueName
+                | UniqueEmail
+                | FieldTooLong Int
+                | NomyxCFE (CommonFormError [HS.Input])
+                deriving Show
+
+type NomyxForm a = Form (ServerPartT IO) [HS.Input] NomyxError Html () a
+
+default (Integer, Double, Data.Text.Text)
+
+data LoginName = LoginName { login :: PlayerName}
+                             deriving (Show, Eq)
+
+
+-- | associate a player number with a handle
+data PlayerClient = PlayerClient PlayerNumber deriving (Eq, Show)
+
+-- | A structure to hold the active games and players
+data Server = Server [PlayerClient] deriving (Eq, Show)
+
+data PlayerCommand = Auth AuthenticateURL
+                   | Login
+                   | Logout
+                   | ResetPassword
+                   | ChangePassword
+                   | OpenIdRealm
+                   | PostAuth
+                   | MainPage
+                   | JoinGame  GameName
+                   | LeaveGame GameName
+                   | DelGame   GameName
+                   | DoInput   EventNumber SignalAddress FormField GameName
+                   | NewRule   GameName
+                   | NewGame
+                   | SubmitNewGame
+                   | Upload
+                   | Advanced
+                   | SubmitPlayAs GameName
+                   | SubmitAdminPass
+                   | SubmitSettings
+                   | SaveFilePage
+                   | NomyxJS
+                   deriving (Show)
+
+
+--newtype ClckT url m a = ClckT { unClckT :: RouteT url (StateT ClckState m) a }
+
+data WebState = WebState {_session           :: TVar Session,
+                          _authenticateState :: AcidState AuthenticateState,
+                          _routeAuthenticate :: AuthenticateURL -> RouteT AuthenticateURL (ServerPartT IO) Response}
+
+makeLenses ''WebState
+
+type RoutedNomyxServer a = RouteT PlayerCommand (StateT WebState (ServerPartT IO)) a
+
+
+instance PathInfo SignalAddressElem
+instance PathInfo SignalAddress
+instance PathInfo FormField
+instance PathInfo (Int, String)
+instance PathInfo [(Int, String)]
+
+$(derivePathInfo ''PlayerCommand)
+$(derivePathInfo ''LoginName)
+
+instance PathInfo Bool where
+  toPathSegments i = [pack $ show i]
+  fromPathSegments = pToken (const "bool") (checkBool . show)
+    where checkBool str = case reads str of
+            [(n,[])] -> Just n
+            _ ->        Nothing
+
+instance FormError NomyxError where
+    type ErrorInputType NomyxError = [HS.Input]
+    commonFormError = NomyxCFE
+
+instance ToMarkup NomyxError where
+    toMarkup PlayerNameRequired = "Player Name is required"
+    toMarkup GameNameRequired   = "Game Name is required"
+    toMarkup UniqueName         = "Name already taken"
+    toMarkup UniqueEmail        = "Email already taken"
+    toMarkup (FieldTooLong l)   = fromString $ "Field max length: " ++ show l
+    toMarkup (NomyxCFE e)       = fromString $ show e
