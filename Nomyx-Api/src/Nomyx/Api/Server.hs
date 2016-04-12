f5fbc5bdd2761f6201ee18ac258a777463fd9561
Corentin Dupont <corentin.dupont@gmail.com>
2016-04-12 23:06:47 +0200

    cors policy

--- Nomyx-Api/src/Nomyx/Api/Server.hs
+++ Nomyx-Api/src/Nomyx/Api/Server.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Nomyx.Api.Server where
 
@@ -8,16 +9,24 @@ import           Nomyx.Api.Api
 import           Nomyx.Api.DefaultApi
 import           Servant
 import qualified Network.Wai.Handler.Warp as Warp
+import           Network.Wai.Middleware.Cors
+import           Network.Wai
 import           Control.Concurrent.STM
 import           Nomyx.Core.Types
 import           Data.Yaml                           ( encode)
 import qualified Data.ByteString.Char8            as BL
 import           Servant.Swagger
+import           Data.Monoid
 
 serveApi :: TVar Session -> IO ()
 serveApi tv = do
-   putStrLn "Starting Api..."
-   Warp.run 8080 $ serve api $ server tv
+   putStrLn "Starting Api on http://localhost:8001/"
+   Warp.run 8001 $ myCors $ serve api $ server tv
+
+customPolicy = simpleCorsResourcePolicy { corsMethods = simpleMethods <> ["DELETE", "PUT"] }
+
+myCors :: Middleware
+myCors = cors (const $ Just customPolicy)
 
 saveSwaggerYaml :: IO ()
-saveSwaggerYaml = BL.writeFile "swagger.yaml" (encode $ toSwagger api)
+saveSwaggerYaml = BL.writeFile "doc/swagger.yaml" (encode $ toSwagger api)
