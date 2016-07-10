1f4b2bc105a97cd71a7378d31c78180ef1179c99
Corentin Dupont <corentin.dupont@gmail.com>
2016-07-10 11:49:46 +0200

    new library "Imprevu"

---
+++ Imprevu/src/Imprevu/EvMgt.hs
@@ -0,0 +1,113 @@
+{-# LANGUAGE CPP                       #-}
+{-# LANGUAGE DataKinds                 #-}
+{-# LANGUAGE DeriveDataTypeable        #-}
+{-# LANGUAGE DeriveGeneric             #-}
+{-# LANGUAGE EmptyDataDecls            #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE GADTs                     #-}
+{-# LANGUAGE KindSignatures            #-}
+{-# LANGUAGE MultiParamTypeClasses     #-}
+{-# LANGUAGE ScopedTypeVariables       #-}
+{-# LANGUAGE StandaloneDeriving        #-}
+{-# LANGUAGE TemplateHaskell           #-}
+{-# LANGUAGE DeriveGeneric             #-}
+{-# LANGUAGE RankNTypes             #-}
+
+-- | This module contains the type definitions necessary to build a Nomic rule.
+module Imprevu.EvMgt where
+
+import           Control.Applicative hiding (Const)
+import           Control.Lens
+import           Control.Monad.Error
+import           Control.Shortcut
+import           Data.Data           (Data)
+import           Data.Time
+import           Data.List
+import           Data.Typeable
+import           GHC.Generics
+import           System.Random
+import           Imprevu.Events
+import           Imprevu.Internal.EventEval
+import           Imprevu.Internal.Utils
+import           Control.Monad.State hiding (execState)
+import           Control.Lens
+import           Data.Todo
+
+
+-- * Nomyx Expression
+
+class (Typeable n, Monad n, Applicative n) => EvMgt n where
+   --Events management
+   onEvent         :: (Typeable a, Show a) => Event a -> ((EventNumber, a) -> n ()) -> n EventNumber
+   delEvent        :: EventNumber -> n Bool
+   getEvents       :: n [EventInfo n]
+   --sendMessage     :: (Typeable a, Show a) => Msg a -> a -> n ()
+   currentTime     :: n UTCTime
+
+data Msg m     = Msg String deriving (Typeable, Show, Eq)
+
+-- * Evaluation
+
+-- | evaluate an effecful expression.
+
+--evOnEvent :: (Typeable e, Show e, EvMgt n) => Event e -> ((EventNumber, e) -> n ()) -> Evaluate n s EventNumber
+--evOnEvent ev h = do
+--   evs <- use events
+--   let en = getFreeNumber (map _eventNumber evs)
+--   events %= (EventInfo en ev h SActive [] : )
+--   return en
+
+--evSendMessage :: (Typeable a, Show a) => Msg a -> a -> Evaluate ()
+--evSendMessage m = triggerEvent (Signal m)
+--
+--evDelEvent :: EventNumber -> Evaluate Bool
+--evDelEvent en = do
+--   evs <- use events
+--   case find ((== en) . getL eventNumber) evs of
+--      Nothing -> return False
+--      Just eh -> case _evStatus eh of
+--         SActive -> do
+--            events .= replaceWith ((== en) . getL eventNumber) eh{_evStatus = SDeleted} evs
+--            return True
+--         SDeleted -> return False
+--
+--evTriggerTime :: UTCTime -> Evaluate ()
+--evTriggerTime t = triggerEvent (Time t) t
+
+getRemainingSignals :: EventInfo n -> [EventInfo n] -> [(SignalAddress, SomeSignal)]
+getRemainingSignals (EventInfo _ e _ _ en) eis = undefined  --case runEvaluate eis $ runEvalError (getEventResult e en) of
+--   Done _ -> []
+--   Todo a -> a
+
+
+--extract the game state from an Evaluate
+--knowing the rule number performing the evaluation (0 if by the system)
+--and the player number to whom display errors (set to Nothing for all players)
+--TODO: clean
+--runEvalError :: Evaluate n s a -> State (EvalEnv n s) a
+--runEvalError eva = undefined --modify (\g -> _eGame $ execState (runEvalError' mpn egs) (EvalEnv rn g evalNomex evalNomexNE))
+--
+--runEvalError' :: Evaluate a -> State (EvalEnv n s) ()
+--runEvalError' eva = do
+--   e <- runErrorT eva
+--   case e of
+--      Right _ -> return ()
+--      Left e' -> undefined
+--        --tracePN (fromMaybe 0 mpn) $ "Error: " ++ e'
+         --void $ runErrorT $ log mpn "Error: "
+
+--runSystemEval :: Evaluate n s a -> State s a
+--runSystemEval eva = focus execState $ runEvalError eva
+--
+--runSystemEval' :: Evaluate n s a -> State s ()
+--runSystemEval' eva = void $ runSystemEval eva
+
+
+displayEvent :: [EventInfo n] -> EventInfo n -> String
+displayEvent eis ei@(EventInfo en _ _ s envi) =
+   "event num: " ++ (show en) ++
+   --", remaining signals: " ++ (show $ getRemainingSignals ei eis) ++ --TODO: display also event result?
+  -- ", envs: " ++ (show envi) ++
+   ", status: " ++ (show s)
+
---
+++ Imprevu/src/Imprevu/Test.hs
@@ -0,0 +1,86 @@
+
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Imprevu.Test where
+
+import Imprevu.Events
+import Imprevu.Events2
+import Imprevu.Inputs
+import Imprevu.EvMgt
+import Imprevu.Internal.EventEval
+import Imprevu.Internal.Utils
+import Control.Monad.State
+import Control.Monad.Error
+import Data.Typeable
+import Control.Lens
+import Data.Time
+
+--data EventInfoIO = EventInfoIO { unEvents :: [EventInfo EventIO]}
+newtype Events a = Events (Evaluate Events [String] a)
+  deriving (Functor, Applicative, Monad)
+
+
+instance EvMgt Events where
+   onEvent         = evOnEvent
+   delEvent     en = undefined --modify (filter (\a -> _eventNumber a /= en)) >> return True
+   getEvents       = undefined
+   --sendMessage     = undefined -- :: (Typeable a, Show a) => Msg a -> a -> n ()
+   currentTime     = undefined
+
+
+evalEvents :: Events a -> Evaluate Events [String] a
+evalEvents (Events es) = es
+--  withStateT (\(EventInfoIO eis) -> (EvalEnv eis () evalEventIO undefined)) eio
+
+
+--class (Typeable n, Monad n, Applicative n) => EvMgt n where
+--   --Events management
+--   onEvent         :: (Typeable e, Show e) => Event e -> ((EventNumber, e) -> n ()) -> n EventNumber
+--   delEvent        :: EventNumber -> n Bool
+--   getEvents       :: StateT [EventInfo IO] IO [EventInfo n]
+--   sendMessage     :: (Typeable a, Show a) => Msg a -> a -> n ()
+--   currentTime     :: n UTCTime
+--
+--get :: Monad m => StateT s m s
+
+evOnEvent :: (Typeable e, Show e) => Event e -> ((EventNumber, e) -> Events ()) -> Events EventNumber
+evOnEvent ev h = Events $ do
+   evs <- get
+   let en = getFreeNumber (map _eventNumber $ _events evs)
+   modify (\(EvalEnv es s f g) -> (EvalEnv (EventInfo en ev h SActive [] : es) s f g))
+   return en
+
+defaultEvents :: EvalEnv Events [String]
+defaultEvents = EvalEnv [] [] (evalEvents . void) undefined
+
+execEvents :: (Signal e) => Events a -> e -> SignalDataType e -> [String]
+execEvents r f d = _execState $ runIdentity $ flip execStateT defaultEvents $ do
+   r <- runErrorT $ do
+      void $ evalEvents r
+      triggerEvent f d
+      return ()
+   case r of
+      Right a -> return a
+      Left e -> error $ show "error occured"
+   --putStrLn' $ show s
+
+
+putStrLn' :: String -> Events ()
+putStrLn' s = Events $ do
+  (EvalEnv is ss f g) <- get
+  put (EvalEnv is (s:ss) f g)
+
+
+data Choice = Holland | Sarkozy deriving (Enum, Typeable, Show, Eq, Bounded)
+
+-- Test input
+testSingleInput :: Events ()
+testSingleInput = void $ onInputRadio_ "Vote for Holland or Sarkozy" [Holland, Sarkozy] h 1 where
+   h a = putStrLn' ("voted for " ++ show a)
+
+testSingleInputEx :: Bool
+testSingleInputEx = "voted for Holland" `elem` g where
+   g = execEvents testSingleInput (Input "Vote for Holland or Sarkozy" (Radio [(Holland, "Holland"), (Sarkozy, "Sarkozy")])) Holland
+
