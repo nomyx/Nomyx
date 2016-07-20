5e3235b3ee8ef252a02fa46f8cb19c4ed9687c52
Corentin Dupont <corentin.dupont@gmail.com>
2016-07-20 23:40:53 +0200

    Imprevu-Happstack first commit

---
+++ Imprevu-Happstack/src/Imprevu/Happstack/Forms.hs
@@ -0,0 +1,145 @@
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE ScopedTypeVariables    #-}
+{-# LANGUAGE RankNTypes   #-}
+{-# LANGUAGE ImpredicativeTypes   #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE ExistentialQuantification    #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+
+module Imprevu.Happstack.Forms where
+
+import Imprevu.Internal.Event
+import Imprevu.Internal.InputEval
+import Imprevu.Events
+import Imprevu.Inputs
+import Imprevu.Happstack.Types
+import Control.Monad
+import Control.Monad.Extra (mapMaybeM)
+import Control.Applicative
+import Data.Monoid
+import Data.Maybe
+import Data.Typeable
+import Data.String
+import           Control.Monad.State
+import Happstack.Server              as HS (Input, Response, ServerPartT, Method (..), methodM,
+                                              ok, seeOther, toResponse)
+import           Text.Blaze.Html5            (ToMarkup, Html, toHtml, a, br, div, h3, h4,
+                                              pre, table, td, toValue, tr, (!), input)
+import qualified Text.Blaze.Html5    as H    (form, label)
+import           Text.Blaze.Html5.Attributes as A (id, href, type_, name, value, checked, for, action, method, enctype)
+import           Text.Reform                 (eitherForm, viewForm, (<++), CommonFormError, ErrorInputType,
+                                                Form, FormError (..), FormInput)
+import           Text.Reform.Blaze.String    (inputCheckboxes, label, textarea)
+import qualified Text.Reform.Blaze.String    as RB
+import           Text.Reform.Happstack       (environment)
+import           Web.Routes.RouteT           (liftRouteT, RouteT)
+import qualified Text.Reform.Generalized             as G
+import           Web.Routes.Base
+import           Web.Routes.Happstack                ()
+import           Web.Routes.RouteT
+import           Web.Routes.PathInfo
+import  Data.Text                           (Text)
+default (Integer, Double, Data.Text.Text)
+
+
+viewInput :: EventInfo n -> RoutedServer (Maybe Html)
+viewInput ei@(EventInfo en _ _ SActive _) = do
+   ds <- mapMaybeM (viewInput' en) (getRemainingSignals ei undefined)
+   return $ if null ds
+      then Nothing
+      else Just $ sequence_ ds
+viewInput _ = return Nothing
+
+data SomeData' = forall e. (Typeable e, Eq e) => SomeData' e
+
+(===) :: (Typeable a, Typeable b, Eq a, Eq b) => a -> b -> Bool
+(===) x y = cast x == Just y
+
+test :: SomeData' -> Bool
+test (SomeData' a) = if (Nothing == Nothing) then True else False
+
+
+viewInput' :: EventNumber -> (SignalAddress, SomeSignal) -> RoutedServer (Maybe Html)
+viewInput' en (fa, ss@(SomeSignal a)) = case (cast ss) of
+    Just ((undefined )) -> undefined --do -- (Input title _))) = do
+--  lf  <- liftRouteT $ viewForm "user" $ inputForm ev
+--  let link = showRelURL (DoInput en fa (fromJust $ getFormField ev))
+--  return $ Just $ tr $ td $ do
+--     fromString title
+--     fromString " "
+--     blazeForm lf link ! A.id "InputForm"
+--viewInput' _ _ = return Nothing
+
+
+--f :: SomeSignal -> Maybe (Imprevu.Inputs.Input ())
+--f (SomeSignal a) = case (toConstr a) of
+--   (toConstr (Input _ _)) -> undefined
+
+--- TODO: merge SomeSignal and FormField...
+inputForm :: Imprevu.Inputs.Input a -> ImpForm InputData
+inputForm ((Input _ (Radio choices)))    = RadioData    <$> (reformInputRadio' (zip [0..] (snd <$> choices)) (== 0) <++ label (" " :: String))
+inputForm ((Input _ Text))               = TextData     <$> RB.inputText "" <++ label (" " :: String)
+inputForm ((Input _ TextArea))           = TextAreaData <$> textarea 50 5  "" <++ label (" " :: String)
+inputForm ((Input _ Button))             = pure ButtonData
+inputForm ((Input _ (Checkbox choices))) = CheckboxData <$> inputCheckboxes (zip [0..] (snd <$> choices)) (const False) <++ label (" " :: String)
+inputForm _ = error "Not an input form"
+
+
+inputForm' :: FormField -> ImpForm InputData
+inputForm' (RadioField _ choices)    = RadioData    <$> (RB.inputRadio choices (== 0)) <++ RB.label (" " :: String)
+inputForm' (TextField _)             = TextData     <$> RB.inputText "" <++ label (" " :: String)
+inputForm' (TextAreaField _)         = TextAreaData <$> textarea 50 5  "" <++ label (" " :: String)
+inputForm' (ButtonField _)           = pure ButtonData
+inputForm' (CheckboxField _ choices) = CheckboxData <$> inputCheckboxes choices (const False) <++ label (" " :: String)
+
+--getFormField :: SomeSignal -> Maybe FormField
+--getFormField (SomeSignal a) = case cast a of
+--   Just ((Input _ (Radio choices)) ) -> Just $ RadioField    "" (zip [0..] (snd <$> choices))
+----   Just (Text :: InputForm String)          -> Just $ TextField     ""
+----   Just (TextArea)      -> Just $ TextAreaField ""
+----   Just (Button)        -> Just $ ButtonField   ""
+----   Just (Checkbox choices) -> Just $ CheckboxField "" (zip [1..] (snd <$> choices))
+--getFormField _ = Nothing
+-- | a form result has been sent
+newInput :: EventNumber -> SignalAddress -> FormField -> RoutedServer Response
+newInput en fa ft = toResponse <$> do
+   methodM POST
+   r <- liftRouteT $ eitherForm environment "user" (inputForm' ft)
+   case r of
+      (Right c) -> webCommand $ inputResult en fa ft c
+      (Left _) ->  liftIO $ putStrLn "cannot retrieve form data"
+   seeOther (showRelURL $ Main) "Redirecting..."
+
+showRelURL :: Command -> Text
+showRelURL c = "/Nomyx" <> (toPathInfo c)
+
+-- | Create a group of radio elements without BR between elements
+reformInputRadio' :: (Functor m, Monad m, FormError error, ErrorInputType error ~ input, FormInput input, ToMarkup lbl) =>
+              [(a, lbl)]  -- ^ value, label, initially checked
+           -> (a -> Bool) -- ^ isDefault
+           -> Form m input error Html () a
+reformInputRadio' choices isDefault =
+   G.inputChoice isDefault choices mkRadios
+   where
+      mkRadios nm choices' = mconcat $ concatMap (mkRadio nm) choices'
+      mkRadio nm (i, val, lbl, checked) =
+         [ (if checked then (! A.checked "checked") else Prelude.id) $ input ! A.type_ "radio" ! A.id (toValue i) ! A.name (toValue nm) ! A.value (toValue val)
+         , " ", H.label ! A.for (toValue i) $ toHtml lbl]
+
+
+blazeForm :: Html -> Text -> Html
+blazeForm html link =
+    H.form ! A.action (toValue link)
+         ! A.method "POST"
+         ! A.enctype "multipart/form-data" $
+            do html
+               input ! A.type_ "submit" ! A.value "Submit"
+
+inputResult :: EventNumber -> SignalAddress -> FormField -> InputData -> StateT s IO ()
+inputResult en fa ft ir = undefined --inGameDo gn $ execGameEvent $ InputResult pn en fa ft ir
+
+webCommand :: StateT s IO () -> RoutedServer ()
+webCommand ss = undefined --do
+--  ts <- use session
+--  liftIO $ updateSession ts ss
---
+++ Imprevu-Happstack/src/Imprevu/Happstack/Types.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE ScopedTypeVariables    #-}
+{-# LANGUAGE RankNTypes   #-}
+{-# LANGUAGE ImpredicativeTypes   #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeSynonymInstances   #-}
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE ExistentialQuantification    #-}
+
+module Imprevu.Happstack.Types where
+
+import Imprevu.Internal.Event
+import Imprevu.Internal.InputEval
+import Imprevu.Events
+import Imprevu.Inputs
+import Control.Monad
+import Control.Monad.Extra (mapMaybeM)
+import Control.Applicative
+import Data.Monoid
+import Data.Maybe
+import Data.Typeable
+import Data.String
+import           Control.Monad.State
+import Happstack.Server              as HS (Input, Response, ServerPartT, Method (..), methodM,
+                                              ok, seeOther, toResponse)
+import           Text.Blaze.Html5            (ToMarkup, Html, toHtml, a, br, div, h3, h4,
+                                              pre, table, td, toValue, tr, (!), input)
+import qualified Text.Blaze.Html5    as H    (form, label)
+import           Text.Blaze.Html5.Attributes as A (id, href, type_, name, value, checked, for, action, method, enctype)
+import           Text.Reform                 (eitherForm, viewForm, (<++), CommonFormError, ErrorInputType,
+                                                Form, FormError (..), FormInput)
+import           Text.Reform.Blaze.String    (inputCheckboxes, label, textarea)
+import qualified Text.Reform.Blaze.String    as RB
+import           Text.Reform.Happstack       (environment)
+import           Web.Routes.RouteT           (liftRouteT, RouteT)
+import qualified Text.Reform.Generalized             as G
+import           Web.Routes.Base
+import           Web.Routes.Happstack                ()
+import           Web.Routes.RouteT
+import           Web.Routes.PathInfo
+import           Web.Routes.TH
+import  Data.Text                           (Text)
+default (Integer, Double, Data.Text.Text)
+
+type RoutedServer a = RouteT Command (ServerPartT IO) a
+
+data Command =
+    Main
+  | DoInput   EventNumber SignalAddress FormField
+  deriving (Show)
+
+
+data ImpFormError = ImpFormError (CommonFormError [HS.Input])
+
+instance FormError ImpFormError where
+    type ErrorInputType ImpFormError = [HS.Input]
+    commonFormError = ImpFormError
+
+--instance ToMarkup ImpError where
+--    toMarkup (ImpFormError e)       = fromString $ show e
+
+type ImpForm a = Form (ServerPartT IO) [HS.Input] ImpFormError Html () a
+
+instance PathInfo SignalAddressElem
+instance PathInfo SignalAddress
+instance PathInfo FormField
+instance PathInfo (Int, String)
+instance PathInfo [(Int, String)]
+$(derivePathInfo ''Command)
