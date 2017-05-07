e380abb0f0ccb70129a7a83b65aa71c9112bcdb9
Corentin Dupont <corentin.dupont@gmail.com>
2017-05-07 14:48:39 +0200

    clean

--- launchNomyx.sh
+++
@@ -1,7 +0,0 @@
-
-version=0.7.3
-cmd="cabal exec .cabal-sandbox/bin/Nomyx -- -h www.nomyx.net -p 8000 -r ~/Nomyx/ -f ./.cabal-sandbox/share/x86_64-linux-ghc-7.6.2/Nomyx-Web-$(version)/ -s ./.cabal-sandbox/share/doc/x86_64-linux-ghc-7.6.2/Nomyx-Language-$(version)/ -a XXXX -m"
-
-screen -S nomyx $cmd
-
-
